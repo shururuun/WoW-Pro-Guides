41ec9e813578449c554d298b75401739ce42e1c5
Shururu Ruun <shururuun@gmail.com>
2019-06-20 21:15:31 +0200

    Classic: 25-26 Barrens: Fix PRE QIDs

--- WoWPro_Leveling/Classic/Horde/25_26_Jame_The_Barrens.lua
+++ WoWPro_Leveling/Classic/Horde/25_26_Jame_The_Barrens.lua
@@ -12,21 +12,21 @@ N Poison Quest Chain|QID|2460|C|Rogue|N|While you don't need to do this chain an
 A The Shattered Salute|QID|2460|O|C|Rogue|
 C The Shattered Salute|QID|2460|O|N|Target Senthul and type "/salute".|C|Rogue|
 T The Shattered Salute|QID|2460|O|C|Rogue|
-A Deep Cover|QID|2458|C|Rogue|O|PRE|The Shattered Salute|
+A Deep Cover|QID|2458|C|Rogue|O|PRE|2460|
 
 A Speak with Ruga|QID|1823|N||C|Warrior|Z|Orgrimmar|M|80.2,32.4|
 
 F The Crossroads |QID|5052|N||Z|Orgrimmar|M|45.3,63.9|
 A Egg Hunt |QID|868|M|51.1,29.6|
 
-T Deep Cover|QID|2458|U|8051|O|PRE|The Shattered Salute|N|Head north of the Sludge Fen. When the tower is in sight, use the Flare Gun to signal your arrival to Taskmaster Fizzule. Fire off TWO flares, then approach Taskmaster Fizzule and perform the Shattered /Salute.|C|Rogue|
-A Mission: Possible But Not Probable|QID|2478|O|PRE|Deep Cover|C|Rogue|
-N Pickpocket Silixiz|QID|2478|L|8072|C|Rogue|O|PRE|Deep Cover|N|Pickpocket Foreman Silixiz |M|54.8,6.1|
-K Mutated Venture Co. Drones|QID|2478|C|Rogue|O|PRE|Deep Cover|Q|Mission: Possible But Not Probable|QO|Mutated Venture Co. Drone slain: 2/2|N|Go inside the tower and kill 2 drones on the first floor. They are weak to Ambush.|
-K Mutated Venture Co. Patrollers|QID|2478|C|Rogue|O|PRE|Deep Cover|Q|Mission: Possible But Not Probable|QO|Venture Co. Patroller slain: 2/2|N|Go up the stairs to the second floor and kill 2 patrollers. They are weak to Rupture.|
-K Mutated Venture Co. Lookouts|QID|2478|C|Rogue|O|PRE|Deep Cover|Q|Mission: Possible But Not Probable|QO|Venture Co. Lookout slain: 2/2|N|Go up the stairs to the third floor and kill 2 lookouts. They are weak to Eviscerate.|
-C Mission: Possible But Not Probable|QID|2478|C|Rogue|O|PRE|Deep Cover|N|Stealth up to the fourth floor. Use Ambush on Gallywix, then kill him. Loot his head, and pick the lock on the footlocker. Loot the mixture inside.|
-T Mission: Possible But Not Probable|QID|2478|C|Rogue|O|PRE|Deep Cover|N|Out of the tower and back to Fizzule. Use the whistle to call him.|U|8066|
+T Deep Cover|QID|2458|U|8051|O|N|Head north of the Sludge Fen. When the tower is in sight, use the Flare Gun to signal your arrival to Taskmaster Fizzule. Fire off TWO flares, then approach Taskmaster Fizzule and perform the Shattered /Salute.|C|Rogue|
+A Mission: Possible But Not Probable|QID|2478|O|PRE|2458|C|Rogue|
+N Pickpocket Silixiz|QID|2478|L|8072|C|Rogue|O|N|Pickpocket Foreman Silixiz |M|54.8,6.1|
+K Mutated Venture Co. Drones|QID|2478|C|Rogue|O|Q|Mission: Possible But Not Probable|QO|Mutated Venture Co. Drone slain: 2/2|N|Go inside the tower and kill 2 drones on the first floor. They are weak to Ambush.|
+K Mutated Venture Co. Patrollers|QID|2478|C|Rogue|O|Q|Mission: Possible But Not Probable|QO|Venture Co. Patroller slain: 2/2|N|Go up the stairs to the second floor and kill 2 patrollers. They are weak to Rupture.|
+K Mutated Venture Co. Lookouts|QID|2478|C|Rogue|O|Q|Mission: Possible But Not Probable|QO|Venture Co. Lookout slain: 2/2|N|Go up the stairs to the third floor and kill 2 lookouts. They are weak to Eviscerate.|
+C Mission: Possible But Not Probable|QID|2478|C|Rogue|O|N|Stealth up to the fourth floor. Use Ambush on Gallywix, then kill him. Loot his head, and pick the lock on the footlocker. Loot the mixture inside.|
+T Mission: Possible But Not Probable|QID|2478|C|Rogue|O|N|Out of the tower and back to Fizzule. Use the whistle to call him.|U|8066|
 
 F Camp Taurajo |QID|5052|N| For those who don't have the flight path: From The Crossroads follow the path all the way south until the road forks. Camp Taurajo is right between the two paths.|M|51.5,30.3|
 A Tribes at War |QID|878|M|44.6,59.2|
@@ -42,7 +42,7 @@ K Kill Bristlebacks for Blood Shard |QID|5052|QO|Blood Shard: 1/1| |N| You will 
 T Blood Shards of Agamaggan |QID|5052|M|44.6,59.2|
 A Betrayal from Within (Part 1)|QID|879|M|44.6,59.2|
 A Weapons of Choice |QID|893|M|45.1,57.7|
-A Mahren Skyseer |QID|874|O|PRE|Cry of the Thunderhawk|
+A Mahren Skyseer |QID|874|O|PRE|913|
 
 T Call of Water (Part 5)|QID|1534|C|Shaman|M|43.4,77.4|
 A Call of Water (Part 6)|QID|220|C|Shaman|M|43.4,77.4|
@@ -95,9 +95,9 @@ F Ratchet |QID|6571|N||M|51.5,30.3|
 T Call of Water (Part 6)|QID|220|C|Shaman|M|65.8,43.8|
 A Call of Water (Part 7)|QID|63|C|Shaman|M|65.8,43.8|
 T Mahren Skyseer |QID|874|O|
-A Isha Awak |QID|873|O|PRE|Mahren Skyseer|
-K Kill Isha Awak for Heart of Isha Awak |QID|873|QO|Heart of Isha Awak: 1/1|O|PRE|Mahren Skyseer|N||M|64,50|
-T Isha Awak |QID|873|O|PRE|Mahren Skyseer|
+A Isha Awak |QID|873|O|PRE|874|
+K Kill Isha Awak for Heart of Isha Awak |QID|873|QO|Heart of Isha Awak: 1/1|O|M|64,50|
+T Isha Awak |QID|873|O|
 
 b Booty Bay|QID|6571|N||M|63.7,38.6|
 N Loot Warsong Axe Shipment |QID|6571|QO|Warsong Axe Shipment: 1/1|N||Z|Stranglethorn Vale|M|26.4,73.3|
